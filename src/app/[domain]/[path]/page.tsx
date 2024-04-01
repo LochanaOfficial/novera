041dbd62508ac5640dc2e4f4fcff2fe92e8e83d7
LochanaOfficial <lochanarathnayake30@gmail.com>
2024-04-01 18:42:58 +0530

    moving to production5

--- src/app/[domain]/[path]/page.tsx
+++
@@ -1,8 +0,0 @@
-const Page = () => {
-  return (
-    <div>Path Page</div>
-  )
-}
-
-
-export default Page;
--- src/app/[domain]/page.tsx
+++
@@ -1,8 +0,0 @@
-const Page = () => {
-  return (
-    <div>Domain Page</div>
-  )
-}
-
-
-export default Page;
